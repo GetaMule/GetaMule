faed6751b68481e3c88cb28951da7fb6918ddf43
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-03-08 12:43:52 +0100

    Done

--- client/src/app/message/message.component.ts
+++ client/src/app/message/message.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SessionService } from "../../services/session.service";
 import { SubmitProdService } from "../../services/submit-prod.service";
 import { ActivatedRoute, Router } from "@angular/router";
+import { HomeComponent } from '../home/home.component'
 
 @Component({
   selector: 'app-message',
@@ -14,15 +15,17 @@ export class MessageComponent implements OnInit {
   subject: string
   text: string
   html: string
+  values: string
   constructor(public customerService: SubmitProdService,
     private router: Router,
     private route: ActivatedRoute,
     public session: SessionService) { }
   ngOnInit() {
+      
   }
   sendEmail(){
     console.log('entro en el componente')
-    this.usermail = "lucasmartincalderon1@gmail.com"
+    this.usermail = "lucasmartincalderon1@gmail.com, l6cenrique@gmail.com"
     console.log(this.usermail)
     this.customerService.sendEmail(this.usermail, this.subject, this.text, this.html).subscribe(res => {
       console.log('Sent!');
